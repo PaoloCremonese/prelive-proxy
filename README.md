c287b86325a7223627336384ff8875710880e0de
Paolo Cremonese <paolo.a.cremonese@gmail.com>
2023-09-21 16:38:55 +0200

    21/09/2023 16:39

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Prelive with proxy structure for Dockered deploy
 # Configurazione
 1. va aggiunta la chiave privata teledata.website.key in WildflyContainerSrc_thirdtry
 2. Nel FileSystem della macchina ospite si deve creare una cartella
-        /home/teledata/prelive/TeleCLOUD/prelive/nginx/certificati
+        /home/teledata/prelive-proxy/TeleCLOUD/prelive/nginx/certificati
    dove vanno copiati i certificati letsencrypt:
         fullchain.pem
         privkey.pem
--- build.sh
+++ build.sh
@@ -6,17 +6,17 @@ echo "Build PRELIVE containers"
 
 # 1.  stop containers
 echo "Stop containers"
-cd /home/teledata/prelive
+cd /home/teledata/prelive-proxy
 docker compose down
 
 # 2.  supposing all the files has been placed in the right place, build the container
 echo "Build Wildfly"
-cd /home/teledata/prelive/WildflyContainerSrc_thirdtry
+cd /home/teledata/prelive-proxy/WildflyContainerSrc_thirdtry
 docker build paolocremonese/wf-9.0.2.final:thirdtry .
 
 # 3.  start the container again
 echo "Start containers again"
-cd /home/teledata/prelive
+cd /home/teledata/prelive-proxy
 docker compose up --detach
 
 echo "DONE !!!"
